dc31b1c971af549a6d4bdf2d64caa84ecaf45671
Jaap Huisman <jhuisman@hostnet.nl>
2018-07-07 11:39:37 +0200

    Change include arduino.h to Arduino.h to avoid 'No such file or directory' errors

--- Firmware/Source code/Hardware Version 2.0 (HW2.0)/NixieClockShield_NCS314/doIndication314_HW2.x.h
+++ Firmware/Source code/Hardware Version 2.0 (HW2.0)/NixieClockShield_NCS314/doIndication314_HW2.x.h
@@ -1,7 +1,7 @@
 #ifndef __DOINDICATION_H
 #define __DOINDICATION_H
 //#include <stdint.h>
-#include <arduino.h>
+#include <Arduino.h>
 unsigned int SymbolArray[10]={1, 2, 4, 8, 16, 32, 64, 128, 256, 512};
 const unsigned int fpsLimit=16666;
 const byte LEpin=10;
