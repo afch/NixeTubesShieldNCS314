ccc8e3117fafdaca05e63ca07bb092327fb9f821
Aleksej Fomin <fominalec@gmail.com>
2023-05-08 16:32:05 +0300

    Update NCS314_v3_SimpleSample.ino
    Fixed a bug with tubes freezing

--- Firmware/Source code/Hardware Version 3.x (HW3.x)/NCS314_v3_SimpleSample/NCS314_v3_SimpleSample.ino
+++ Firmware/Source code/Hardware Version 3.x (HW3.x)/NCS314_v3_SimpleSample/NCS314_v3_SimpleSample.ino
@@ -1,10 +1,13 @@
 //NIXIE CLOCK SHIELD NCS314 v 3.x by GRA & AFCH (fominalec@gmail.com)
+//1.1 08.05.2023
+//fixed: bug with SHDN pin
 //1 25.04.2023
-//Simple code for HV57708 Hihg Voltage registers
+//Simple code for HV57708 High Voltage registers
  
 #include <SPI.h>
 
 const byte LEpin=10; //pin Latch Enabled data accepted while HI level
+#define pinSHDN 5 // turns off tubes when LOW
 
 String stringToDisplay="000000";// Conten of this string will be displayed on tubes (must be 6 chars length)
 
@@ -17,30 +20,32 @@ Init Programm
 *******************************************************************************************************/
 void setup() 
 {
- 
-  Serial.begin(115200);
+   Serial.begin(115200);
   Serial.println(F("Starting"));
   
   pinMode(LEpin, OUTPUT);
+  
+  pinMode(pinSHDN, OUTPUT);
+  digitalWrite(pinSHDN, HIGH);  //turn on the lamps
 
  // SPI setup
   SPI.begin();  
-  SPI.beginTransaction(SPISettings(1000000, MSBFIRST /*LSBFIRST*/, SPI_MODE3));
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE3));
 }
 
 /***************************************************************************************************************
 MAIN Programm
 ***************************************************************************************************************/
 void loop() {
-  stringToDisplay="123456";
-  Serial.println(stringToDisplay);
-  doIndication();
-  delay(3000);
-  stringToDisplay="654321";
-  Serial.println(stringToDisplay);
-  doIndication();
-  delay(3000);
-}
+  stringToDisplay="123456"; //set numbers to output
+  Serial.println(stringToDisplay); //output given digits to serial port (optional)
+  doIndication(); //display previously set numbers on the tubes
+  delay(3000); //three second delay
+  stringToDisplay="654321"; //set new numbers to output (just for fun)
+  Serial.println(stringToDisplay); //output given digits to serial port (optional)
+  doIndication(); //display previously set numbers on the tubes
+  delay(3000); //three second delay
+} // returns to the first line of the loop function, which causes the digits to change from 123456 to 654321 indefinitely every three seconds.
 
 
 void doIndication()
